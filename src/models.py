fe54f65285053723fdb25e033c6b23b3e8f167c1
Ivan Yevpat <maracasabat@gmail.com>
2022-09-26 17:12:39 +0300

    fixes

--- src/models.py
+++ src/models.py
@@ -5,24 +5,27 @@ from src import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    address = db.Column('address', db.String(120), nullable=True)
-    phone = relationship('Phone', back_populates='user', cascade="all, delete-orphan")
-    email = relationship('Email', back_populates='user', cascade='all, delete-orphan')
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(100), nullable=False)
+    phone = db.Column('phone', db.String(20), nullable=False)
+    email = db.Column('email', db.String(100), nullable=True)
+    address = db.Column('address', db.String(100), nullable=True)
 
+    # phone = relationship('Phone', back_populates='user', cascade="all, delete-orphan")
+    # email = relationship('Email', back_populates='user', cascade='all, delete-orphan')
 
-class Phone(db.Model):
-    __tablename__ = 'phones'
-    id = db.Column(db.Integer, primary_key=True)
-    phone_number = db.Column('phone_number', db.String(20), nullable=False)
-    user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
-    user = relationship('User', back_populates='phone')
-
-
-class Email(db.Model):
-    __tablename__ = 'emails'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column('email', db.String(120), nullable=False)
-    user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
-    user = relationship('User', back_populates='email')
+#
+# class Phone(db.Model):
+#     __tablename__ = 'phones'
+#     id = db.Column(db.Integer, primary_key=True)
+#     phone_number = db.Column('phone_number', db.String(20), nullable=False)
+#     user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
+#     user = relationship('User', back_populates='phone')
+#
+#
+# class Email(db.Model):
+#     __tablename__ = 'emails'
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column('email', db.String(120), nullable=False)
+#     user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
+#     user = relationship('User', back_populates='email')
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
-from src.models import User, Phone, Email
+from src.models import User
 from src import db
-from sqlalchemy import update
 from sqlalchemy.orm import joinedload
 from . import app
 
@@ -13,50 +12,51 @@ def healthcheck():
 
 @app.route('/', strict_slashes=False)
 def index():
-    contacts = User.query.options(joinedload(User.phone), joinedload(User.email)).all()
+    contacts = User.query.all()
     return render_template('index.html', contacts=contacts)
 
 
 @app.route('/detail/<int:id>', strict_slashes=False)
 def detail(id):
-    contact = User.query.options(joinedload(User.phone), joinedload(User.email)).filter_by(id=id).first()
+    contact = User.query.get(id)
     return render_template('detail.html', contact=contact)
 
 
 @app.route('/contact', methods=['GET', 'POST'], strict_slashes=False)
 def add_user():
     if request.method == 'POST':
-        user = User(name=request.form['name'], address=request.form['address'])
-        phone = Phone(phone_number=request.form['phone'])
-        email = Email(email=request.form['email'])
-        user.phone.append(phone)
-        user.email.append(email)
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']
+        address = request.form['address']
+        user = User(name=name, phone=phone, email=email, address=address)
         db.session.add(user)
         db.session.commit()
-        flash('Contact added successfully')
+        flash('Contact added successfully!')
         return redirect(url_for('index'))
     return render_template('contact.html')
 
 
+#
 @app.route('/delete/<int:id>', strict_slashes=False)
 def delete(id):
     user = User.query.get(id)
     db.session.delete(user)
     db.session.commit()
-    flash('Contact deleted successfully')
+    flash('Contact deleted successfully!')
     return redirect(url_for('index'))
 
 
+#
 @app.route('/update/<int:id>', methods=['GET', 'POST'], strict_slashes=False)
 def update(id):
     user = User.query.get(id)
-    phone = Phone.query.get(id)
-    email = Email.query.get(id)
     if request.method == 'POST':
         user.name = request.form['name']
+        user.phone = request.form['phone']
+        user.email = request.form['email']
         user.address = request.form['address']
-        phone.phone_number = request.form['phone']
-        email.email = request.form['email']
         db.session.commit()
-        flash('Contact updated successfully')
+        flash('Contact updated successfully!')
         return redirect(url_for('index'))
+    return render_template('update.html', user=user)
