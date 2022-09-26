5d0efbd9836e55f17a3eb88444b11dc31e83218d
Ivan Yevpat <maracasabat@gmail.com>
2022-09-26 17:14:47 +0300

    fixes

--- src/models.py
+++ src/models.py
@@ -1,5 +1,3 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 from src import db
 
 
@@ -11,21 +9,3 @@ class User(db.Model):
     email = db.Column('email', db.String(100), nullable=True)
     address = db.Column('address', db.String(100), nullable=True)
 
-    # phone = relationship('Phone', back_populates='user', cascade="all, delete-orphan")
-    # email = relationship('Email', back_populates='user', cascade='all, delete-orphan')
-
-#
-# class Phone(db.Model):
-#     __tablename__ = 'phones'
-#     id = db.Column(db.Integer, primary_key=True)
-#     phone_number = db.Column('phone_number', db.String(20), nullable=False)
-#     user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
-#     user = relationship('User', back_populates='phone')
-#
-#
-# class Email(db.Model):
-#     __tablename__ = 'emails'
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column('email', db.String(120), nullable=False)
-#     user_id = db.Column('user_id', db.Integer, ForeignKey('users.id'), nullable=False)
-#     user = relationship('User', back_populates='email')
